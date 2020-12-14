ec0da6db648829b3b8e33f5908fb6c99c4020f58
honpio <kumamann4@gmail.com>
2020-12-14 20:20:12 +0900

    dlgate_tableSeederTest unitテスト作成

--- laravel/database/seeds/usersTableSeeder.php
+++ laravel/database/seeds/usersTableSeeder.php
@@ -24,7 +24,7 @@ class usersTableSeeder extends Seeder
             [
             'name' =>'test',
             'email'=>'test@exmple.com',
-            'email_verified_at'=>date('Y-m-d H:i:s'),
+            'email_verified_at'=>'2020-12-09 14:40:35',
             'password'=>bcrypt('testpass'),
             // 'created_at'=>date('Y-m-d H:i:s'),
             // 'update_at'=>date('Y-m-d H:i:s'),
@@ -32,7 +32,7 @@ class usersTableSeeder extends Seeder
             [
             'name' =>'test2',
             'email'=>'test2@exmple.com',
-            'email_verified_at'=>date('Y-m-d H:i:s'),
+            'email_verified_at'=>'2020-12-09 14:40:35',
             'password'=>bcrypt('testpass'),
             // 'created_at'=>date('Y-m-d H:i:s'),
             // 'update_at'=>date('Y-m-d H:i:s'),
