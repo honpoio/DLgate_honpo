bac48c24c85e074dd008d10625ed48bde9cbbb55
honpio <kumamann4@gmail.com>
2020-12-16 22:29:18 +0900

    Gate_CRUDControllerLoginTestの修正

--- laravel/tests/Feature/Gate_CRUDControllerLoginTest.php
+++ laravel/tests/Feature/Gate_CRUDControllerLoginTest.php
@@ -9,7 +9,7 @@ use App\User;
 use App\Dlgate_Table;
 use Illuminate\Support\Facades\Auth;
 
-class CRUDControllerLoginTest extends TestCase
+class Gate_CRUDControllerLoginTest extends TestCase
 {
 
 
@@ -32,33 +32,51 @@ class CRUDControllerLoginTest extends TestCase
 
     public function testGateLoginForm(){
         // 　/Dlgateでログインの有無を検証する
+
+        $this->get('DLgate')->assertRedirect('login')->assertStatus(302);
+        //ログインしていない状態で/DLgateにアクセスする
+
+
         $response = $this->actingAs(User::find(1))
         ->get('DLgate');
             
         $this->assertTrue(Auth::check());
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+        ->assertViewIs('DLgate');
+
+        
     }
     public function testGateLoginUpdateForm(){
-        // /Dlgate/update?URL_id=(URL_id)を検証
+        // /Dlgate/update?URL_id=(URL_id)のログインの有無を検証
+
         $dlgate_table = Dlgate_Table::select(['URL_id'])->where('id',[1])->get();
         foreach($dlgate_table as $row){
             $URL_id = $row->URL_id;
+        //動的パスのupdate?URL_id={}を取得
         }
-        
+
+        $this->get('update?URL_id='.$URL_id)->assertRedirect('login')->assertStatus(302);
+        //ログインしていない状態で/DLgateにアクセスする
+
         $response = $this->actingAs(User::find(1))
         ->get('update?URL_id='.$URL_id);
-
-
         $this->assertTrue(Auth::check());
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+        ->assertViewIs('DLgate_update');
     }
     
     public function testGateLoginInsertForm(){
+        //DLgate/createパスのログイン有無を検証
+
+        $this->get('DLgate/create')->assertRedirect('login')->assertStatus(302);
 
         $response = $this->actingAs(User::find(1))
         ->get('DLgate/create');
             
         $this->assertTrue(Auth::check());
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+        ->assertViewIs('DLgate_create');
     }
+
+    
 }
