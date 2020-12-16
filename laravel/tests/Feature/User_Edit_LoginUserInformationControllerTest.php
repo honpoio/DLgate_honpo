163e5b43f04e77793d27361dfe2794b6a1f8eafe
honpio <kumamann4@gmail.com>
2020-12-16 22:51:23 +0900

    User_Edit_LoginUserInfromationControllerTestにコメントを追記

--- laravel/tests/Feature/User_Edit_LoginUserInformationControllerTest.php
+++ laravel/tests/Feature/User_Edit_LoginUserInformationControllerTest.php
@@ -25,21 +25,25 @@ class User_Edit_LoginUserInformationControllerTest extends TestCase
     }
 
     public function testUserInformation(){
+        // 　/userパスのログインの有無を検証する
         $this->get('user')->assertRedirect('login')->assertStatus(302);
-        //ログインしていない状態で/Userにアクセスする
+        //ログインしていない状態で/userにアクセスする
 
         $response = $this->actingAs(User::find(1))
+        //ログインした状態で/userにアクセス
         ->get('user');
-            
         $this->assertTrue(Auth::check());
         $response->assertStatus(200)
         ->assertViewIs('auth.UserInformationEdit');
     }
     public function testWithdrawalForm(){
+        // 　/user/edit/deleteパスのログインの有無を検証する
         $this->get('/user/edit/delete')->assertRedirect('login')->assertStatus(302);
+        //ログインしていない状態で/user/edit/deleteにアクセス
 
         $response = $this->actingAs(User::find(1))
         ->get('/user/edit/delete');
+        //ログインした状態で/user/edit/deleteにアクセス
 
         $this->assertTrue(Auth::check());
         $response->assertStatus(200)
