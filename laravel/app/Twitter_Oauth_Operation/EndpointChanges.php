8946292dfccabc1e013952f73c44c1d2078ad9fb
honpio <kumamann4@gmail.com>
2020-12-21 12:49:17 +0900

    テストコードのリファクタリング＆テスト完了

--- laravel/app/Twitter_Oauth_Operation/EndpointChanges.php
+++ laravel/app/Twitter_Oauth_Operation/EndpointChanges.php
@@ -10,20 +10,13 @@ class EndpointChanges extends SocialiteManager
     public function Scope_Change($provider,$request){
         if($request->has('Follow')){
             $this->Callback_Change = 'http://localhost:8000/callback';
-            $Twitter_provider = self::driver($provider)->redirect();
-
-            // $_Twitter_provider = $Twitter_provider->headers->headers["location"][0];
-                    // laravel/vendor/symfony/http-foundation/HeaderBag.phpに格納されている
-                    //　headersプロパティを参照するために__getメソッドを追加
-            // return  view('testDLgate',[ 'Twitter_provider' => $_Twitter_provider]);
+            // $Twitter_provider = self::driver($provider)->redirect();
             return self::driver($provider)->redirect();
         }
+
         elseif($request->has('RT')){
             $this->Callback_Change = 'http://localhost:8000/callback2';
-            $Twitter_provider = self::driver($provider)->redirect();
-            // $_Twitter_provider = $Twitter_provider->headers->headers["location"][0];
-            // return  self::driver($provider)->redirect();
-            // return  view('testDLgate',[ 'Twitter_provider' => $_Twitter_provider]);
+            // $Twitter_provider = self::driver($provider)->redirect();
             return self::driver($provider)->redirect();
         }
     }
