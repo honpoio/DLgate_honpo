c924ac6473cc0b38b99e86eb983f7eeac85028b9
honpio <kumamann4@gmail.com>
2020-12-25 22:11:59 +0900

    rideirect_button.vueを追加

--- laravel/resources/views/DLgate.blade.php
+++ laravel/resources/views/DLgate.blade.php
@@ -6,14 +6,13 @@
 
 <html lang="ja">
 <head>
-<!-- [if lt IE 9] -->
-<!-- [endif] -->
+
 </head>
 <body>
 @section('content')
 
 <form method="GET" action="/DLgate/create">
-    <button dusk="view-button" class="btn　btn-primary" >gateを新規作成する</button>
+    <button dusk="view-button" class="button_font_variable_length" >gateを新規作成する</button>
 </form>
 </nav>
 <div class="card">
@@ -21,32 +20,41 @@
 @foreach($dlgate_table as $row)
     <div class="card-header">ゲート名:{{$row->gate_name}}
     <br>
-    作成ゲート数:{{$loop->iteration}}</div>
-    <div>DL_url:{{$row->dl_url}}
-        <div style="display:inline-flex">
-        <form method="GET" action="/DLgate/view">
-            <input type="hidden" name='id' value={{$row->URL_id}}>
-                <button dusk="view-button" class="btn　btn-primary" >view</button>
+        作成ゲート数:{{$loop->iteration}}
+    </div>
+    <div class="card-body">
+        <div class="form-group row">
+            DL_url: {{$row->dl_url}}
         </div>
+        <div class="form-group row">
+            twitterUser: {{$row->Twitter_user}}
+        </div>
+        <div class="form-group row">
+            tweetid: {{$row->Twitter_tweet}}
+        </div>
+    </div>
+        <form method="GET" action="/DLgate/view">
+            <input  type="hidden" name='id' value={{$row->URL_id}}>
+                <button dusk="view-button" class="button_font_variable_length">view</button>
         </form>
-        <hello-world-component test="http://127.0.0.1:8000/DLgate/view?id={{ $row['URL_id'] }}"></hello-world-component>
 
+        
 
+    
+        <redirect_button-component redirect_button="{{url('/')}}/DLgate/view?id={{ $row['URL_id'] }}"></redirect_button-component>
 
     <!-- bodyタグ内の下部に以下を入力する -->
     <form method="GET" action="/update">
         <input type="hidden" name="URL_id" value={{$row["URL_id"]}}>
-        <input type="submit" value="{{$row->gate_name}}をupdateする" style="width:160px">
+        <button dusk="view-button" class="button_font_variable_length">update</button>
     </form>
 
     <form method="POST" action="/delete">
     @csrf
         <input type="hidden" name="_method" value="DELETE">
-        <input type="hidden" name="URL_id" value={{$row["URL_id"]}}  style="width:160px">
-        <input type="submit" value="{{$row->gate_name}}をdeleteする">
+        <input type="hidden" name="URL_id" value={{$row["URL_id"]}}>
+        <button dusk="view-button" class="button_font_variable_length">delete</button>
     </form>
-
-    
 @endforeach
 @endif
 
