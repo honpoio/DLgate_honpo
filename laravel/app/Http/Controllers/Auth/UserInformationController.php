760f9989bf947a0c405077776f2d7840729fe5a4
honpio <kumamann4@gmail.com>
2020-12-28 13:12:50 +0900

    add:comment

--- laravel/app/Http/Controllers/Auth/UserInformationController.php
+++ laravel/app/Http/Controllers/Auth/UserInformationController.php
@@ -18,32 +18,47 @@ class UserInformationController extends Controller
         }        
     }
     
+    private function checkTestUser(){
+        //テストユーザーのみ編集機能を制限
+        if(Auth::user()["name"] === 'test'){
+            return \App::abort(404);
+        }
+
+    }
 
     public function UserInformation(Request $request){
+        //ユーザー編集画面を表示させるメソッド
     $auth = auth::user();
     $this->checkLogin();
     return view('auth.UserInformationEdit',['auth'=>$auth]);
     }
     
     public function EmailUpdate(addEmailRequest $request){
+        //登録メールアドレスを更新するメソッド
         $this->checkLogin();
+        $this->checkTestUser();
         return app('User_DB_Operation')->EmailUpdate($request);
     }    
     
     public function PasswordChange(ChangePasswordRequest $request){
+        //パスワードを変更するメソッド
         $this->checkLogin();
+        $this->checkTestUser();
         $user = Auth::user();
         return app('User_DB_Operation')->PasswordChange($request,$user);
     }
 
     public function WithdrawalForm(){
+        //退会フォームを表示させるメソッド
         $this->checkLogin();
         $auth = auth::user();
         return view('auth.WithdrawalForm',['auth'=>$auth]);
     }
 
     public function Withdrawal(WithdrawalRequest $request){
+        //退会処理を追加するメソッド
         $this->checkLogin();
+        $this->checkTestUser();
         $id = auth::id();
         
         return app('User_DB_Operation')->Withdrawal($request,$id);
--- laravel/database/operateDB/DLgate_Operation_DB.php
+++ laravel/database/operateDB/DLgate_Operation_DB.php
@@ -9,15 +9,18 @@ class DLgate_Operation_DB
 {
 
     public function select($id){
+        //userの全てのレコードを表示
         $dlgate_table = Dlgate_Table::where('user_id', $id)->get();
         return view('DLgate',[ 'dlgate_table' => $dlgate_table]);
     }
     public function select_update($request,$id){
+        //指定された1レコードを表示
         $dlgate_table = Dlgate_Table::where('URL_id',$request->URL_id)
         ->where('user_id', $id)->get();
         return view('DLgate_update',[ 'dlgate_table' => $dlgate_table]);
     }
     public function update($request,$id){
+        //レコードを更新
         Dlgate_Table::where('URL_id',$request->URL_id)
         ->where('user_id', $id)
         ->update([
@@ -29,12 +32,14 @@ class DLgate_Operation_DB
         return redirect('/DLgate');
     }
     public function delete($request,$id){
+        //レコードを削除
         Dlgate_Table::where('URL_id',$request->URL_id)
         ->where('user_id', $id)
         ->delete();
         return redirect('/DLgate');
     }
     public function create($request,$id){
+        //レコードを新規作成
         Dlgate_Table::create([
             'user_id' => $id,
             'URL_id' => uniqid(),
