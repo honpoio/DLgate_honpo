7794781df1530580ebbaef43719e294cffe8b85e
honpio <kumamann4@gmail.com>
2020-12-28 13:34:53 +0900

    add:comment

--- laravel/app/Http/Controllers/TwitterController.php
+++ laravel/app/Http/Controllers/TwitterController.php
@@ -11,18 +11,19 @@ class TwitterController extends Controller
     protected static $Twitter_Config;
     
     private function OAuth_AND_User_Info(){
-         
+    // Oauthのアクセスキーを取得するメソッド
         self::$Twitter_Config = app('User_Info')->Twitter_User_Property();
     }
 
     public function redirect(Request $request){
-        
+        //認可サーバーにアクセス
         $provider = "twitter";
         return app('EndpointChange')->Scope_Change($provider,$request);
     }
     
     public function Twetter_Follow(Request $request)
     {
+        //トークンを使用しリソースサーバーにアクセス
         $this->OAuth_AND_User_Info();
         return app('Twitter_Operation')->Follow_Operation($request);
     }
