aa2906d0b53bb646bd0c9a29dd66d399bbdc5278
honpio <kumamann4@gmail.com>
2020-12-28 11:53:19 +0900

    bug:sessionforget

--- laravel/app/Http/Controllers/DLgate/DLGateDisplayController.php
+++ laravel/app/Http/Controllers/DLgate/DLGateDisplayController.php
@@ -12,26 +12,38 @@ use App\Dlgate_Table;
 class DLGateDisplayController extends Controller
 {
     public function DLGateForm(Request  $request){
+        $request->session()->forget('Twitter_user_sucsess',
+        'Twitter_tweet_sucsess','Twitter_user',
+        'Twitter_tweet','dl_url','URL_id');
         
-        $request->session()->forget('Twitter_user','Twitter_tweet');
         $dlgate_table = Dlgate_Table::where('URL_id', $request["id"])->get();
 
             foreach($dlgate_table as $row){
                 $request->session()->put('gate_name',$row->gate_name);
-
-                $request->session()->put('Twitter_user',$row->Twitter_user);
-                if(empty($request->session()->get('Twitter_user') )){
-                    //もしもtwitterのユーザー名を登録していない場合
-                    $request->session()->put('Twitter_user_sucsess',true);
-                }
-                $request->session()->put('Twitter_tweet',$row->Twitter_tweet);
-                //もしもtweetidをデータに挿入していない場合
-                if(empty($request->session()->get('Twitter_tweet') )){
-                    $request->session()->put('Twitter_tweet_sucsess',true);
-                }
-
                 $request->session()->put('dl_url',$row->dl_url);
                 $request->session()->put('URL_id',$row->URL_id);
+
+                if(empty($row->Twitter_user)){
+                    //もしもtwitterのユーザー名を登録していない場合
+                    session()->forget('Twitter_user');
+                    $request->session()->put('Twitter_user_sucsess',true);
+                }else{
+                    $request->session()->put('Twitter_user',$row->Twitter_user);
+                }
+
+                //もしもtweetidをデータに挿入していない場合
+                if(empty($row->Twitter_tweet)){
+                    session()->forget('Twitter_tweet');
+                    $request->session()->put('Twitter_tweet_sucsess',true);
+                }else{
+                    $request->session()->put('Twitter_tweet',$row->Twitter_tweet);
+                }
+
+
+
+
+
+
 
             }
 
