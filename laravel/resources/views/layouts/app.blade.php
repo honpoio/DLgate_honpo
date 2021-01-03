c170260ae9dc8a9811aa6d6da122d678eb09c575
honpio <kumamann4@gmail.com>
2021-01-03 14:47:43 +0900

    test:css

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="https://dlgate-jp.cyou/public/css/app.css" rel="stylesheet">
+    <link href="https://dlgate-jp.cyou/laravel/public/css/app.css" rel="stylesheet">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 
 </head>
