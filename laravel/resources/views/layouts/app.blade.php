60cbc3584515d5d97b449d643b721a055935c3bd
honpio <kumamann4@gmail.com>
2021-01-03 14:46:17 +0900

    test:css

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -10,14 +10,14 @@
     <title>simple_DLgate</title>
 
     <!-- Scripts -->
-    <script src="js/app.js" defer></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://dlgate-jp.cyou/public/css/app.css" rel="stylesheet">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
 
 </head>
